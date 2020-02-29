d27372937d993f41cadc49a1312efdf96d16ea73
wangliang <745837850@qq.com>
2020-02-29 13:52:19 +0800

    11

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -2,7 +2,7 @@ package com.bjpowernode.git;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1111");
     }
 
 }
